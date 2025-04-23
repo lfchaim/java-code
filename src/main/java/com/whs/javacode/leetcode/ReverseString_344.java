a44c5313053e851d5b91cb41827087a5b744015d
Luis Fernando Chaim <lfchaim@gmail.com>
2025-04-23 18:37:47 -0300

    ReverseString_344

--- src/main/java/com/whs/javacode/leetcode/ReverseString_344.java
+++ src/main/java/com/whs/javacode/leetcode/ReverseString_344.java
@@ -17,12 +17,19 @@ public class ReverseString_344 {
 }
 class Solution {
     public void reverseString(char[] s) {
+/*    	
         for (int i = 0, j = s.length - 1; i < s.length; i++, j--) {
 			if (i < j) {
 				char temp = s[i];
 				s[i] = s[j];
 				s[j] = temp;
 			}
+		}
+*/
+    	for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
 		}
     }
 }
